9edcb04cf4fc0d96965d3fddc12d617ec267dc00
Will Busby <will0609@hotmail.com>
2018-08-10 21:02:19 +0100

    Version 1.0.1 - added BaseComponent, BaseProps and defaultBaseProps to exports

--- react-bem-components.ts
+++ react-bem-components.ts
@@ -1,4 +1,7 @@
 
+export {default as BaseComponent} from './components/BaseComponent';
+export {default as BaseProps} from './components/BaseProps';
+export {baseDefaultProps as baseDefaultProps} from './components/BaseProps';
 export {default as Wrapper} from "./components/Wrapper";
 export {default as Title} from "./components/Title";
 export {default as Text} from "./components/Text";
@@ -6,3 +9,4 @@ export {default as Button} from './components/Button';
 export {default as Link} from './components/Link';
 export {default as Image} from './components/Image';
 export {default as Picture} from './components/Picture';
+
